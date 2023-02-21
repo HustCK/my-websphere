3c37dd98632213d66de3ed3944f14abbd5c980ee
ken.cheng <kencheng@ritchiebros.com>
2023-02-21 14:15:40 +0800

    Initial commit

---
+++ src/test/java/com/example/mywebsphere/MyWebsphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mywebsphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebsphereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
